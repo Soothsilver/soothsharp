79f553bb07a39ef8d105a8db4a2047273dd0d4e2
Petr Hudeček <petrhudecek2010@gmail.com>
2016-05-07 19:39:15 +0200

    Adding contracts and an example

---
+++ Sharpsilver.Contracts/Contract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpsilver.Contracts
+{
+    /// <summary>
+    /// Contains static functions that translate into Silver language constructs, such as preconditions or postconditions.
+    /// </summary>
+    public static class Contract
+    {
+        /// <summary>
+        /// Within contracts, represents the return value of a function or a method, if it is of the type System.Int32.
+        /// </summary>
+        public static int IntegerResult { get; } = 0;
+
+        /// <summary>
+        /// Adds a proof obligation: The verifier must ensure that the specified postcondition is true when this method returns.
+        /// </summary>
+        /// <param name="postcondition">The condition that will be verified to be true at each exit point.</param>
+        public static void Ensures(bool postcondition)
+        {
+        }
+    }
+}
---
+++ Sharpsilver.Contracts/VerifiedAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpsilver.Contracts
+{
+    /// <summary>
+    /// The annotated class or method makes use of formal verification and should be translated into Silver.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class VerifiedAttribute : Attribute
+    {
+    }
+}
