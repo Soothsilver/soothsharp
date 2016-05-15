fbda672d7829fb2888250795b1fa1105f1bf18d6
Petr Hudeček <petrhudecek2010@gmail.com>
2016-05-15 10:31:31 +0200

    Pure attribute, Verified Math

--- Sharpsilver.Contracts/VerifiedAttribute.cs
+++ Sharpsilver.Contracts/VerifiedAttribute.cs
@@ -3,7 +3,7 @@
 namespace Sharpsilver.Contracts
 {
     /// <summary>
-    /// The annotated class or method makes use of formal verification and should be translated into Silver.
+    /// The annotated class or method makes use of formal verification and should be translated into Silver. If a class is annotated, then all member methods except those marked [Unverified] are marked for verification as well.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class VerifiedAttribute : Attribute
