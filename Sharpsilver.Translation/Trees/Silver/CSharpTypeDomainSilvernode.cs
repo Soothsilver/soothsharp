9410445137f7b76d7600bd68d31bc4054bcb44ad
Petr Hudeček <petrhudecek2010@gmail.com>
2016-08-20 16:21:43 +0200

    documenation while in Japanese

--- Sharpsilver.Translation/Trees/Silver/CSharpTypeDomainSilvernode.cs
+++ Sharpsilver.Translation/Trees/Silver/CSharpTypeDomainSilvernode.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 
 namespace Sharpsilver.Translation.Trees.Silver
 {
+    /// <summary>
+    /// Represents the "domain CSharpType" construction that is put into all generated Silver files by the transcompiler. 
+    /// See the documentation for more deatils.
+    /// </summary>
+    /// <seealso cref="Sharpsilver.Translation.Trees.Silver.ComplexSilvernode" />
     public class CSharpTypeDomainSilvernode : ComplexSilvernode
     {
         private readonly HighlevelSequenceSilvernode axioms;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CSharpTypeDomainSilvernode"/> class.
+        /// </summary>
+        /// <param name="axioms">Axioms that should be put inside this domain. Must not be null.</param>
         public CSharpTypeDomainSilvernode(HighlevelSequenceSilvernode axioms) : base(null)
         {
             this.axioms = axioms;
