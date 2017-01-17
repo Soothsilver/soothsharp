1186005babe3fedbe473740ca7a94fce1b15e7ad
Petr Hudeček <petrhudecek2010@gmail.com>
2017-01-17 15:07:26 +0100

    fix docs

--- csverify/Csverify.cs
+++ csverify/Csverify.cs
@@ -72,15 +72,15 @@ namespace Soothsharp.Cs2Sil
                 .Add("?|help|h", "Shows this message.", option => showHelp = option != null)
                 .Add("v|version", $"Shows that the version of this program is {version}.", option => showVersion = option != null)
                 .Add("V|verbose", "Enables verbose mode. In verbose mode, additional debugging information is printed and more details are written for each error message.", option => verbose = option != null)
-                .Add("q|quiet", "Enable quiet mode. In quiet mode, only the resulting Silver code or error messages are shown.", option => quiet = option != null)
+                .Add("q|quiet", "Enable quiet mode. In quiet mode, only the resulting Viper code or error messages are shown.", option => quiet = option != null)
                 .Add("r|reference=", "Adds the {ASSEMBLY.DLL} file as a reference when doing semantic analysis on the code. mscorlib and Soothsharp.Contracts are added automatically.", filename => references.Add(filename))
-                .Add("a|assume=", "Translates the file {CLASS.CS} to Silver and prepends it to the main generated file, but its methods and functions won't be verified - their postconditions will be assumed to be true. ", filename => assumedFiles.Add(filename))   
+                .Add("a|assume=", "Translates the file {CLASS.CS} to Viper and prepends it to the main generated file, but its methods and functions won't be verified - their postconditions will be assumed to be true. ", filename => assumedFiles.Add(filename))   
                 .Add("w|wait", "When the program finishes, it will wait for the user to press any key before terminating.", option => waitAfterwards = option != null)
                 .Add("O|only-annotated", "Only transcompile classes that have the [Verified] attribute, and static methods that have the [Verified] attribute even if their containing classes don't have the [Verified] attribute." , option => onlyAnnotated = option != null)
-                .Add("o|output-file=", "Print the resulting Silver code into the {OUTPUT.SIL} file.", filename => outputSilverFile = filename)
-                .Add("line-numbers", "Print line numbers before the Silver code", ln => lineNumbers = ln != null)
-                .Add("s|silicon", "Use the Silicon backend to verify the Silver code. Use the  \"-s-\" option to disable Silicon verification.", option => useSilicon = option != null)
-                .Add("c|carbon", "Use the Carbon backend to verify the Silver code. Use the  \"-c-\" option to disable Carbon verification.", option => useCarbon = option != null)
+                .Add("o|output-file=", "Print the resulting Viper code into the {OUTPUT.VPR} file.", filename => outputSilverFile = filename)
+                .Add("l|line-numbers", "Print line numbers before the Viper code", ln => lineNumbers = ln != null)
+                .Add("s|silicon", "Use the Silicon backend to verify the Viper code.", option => useSilicon = option != null)
+                .Add("c|carbon", "Use the Carbon backend to verify the Viper code.", option => useCarbon = option != null)
                 ;
             try
             {
@@ -147,7 +147,7 @@ namespace Soothsharp.Cs2Sil
             Console.WriteLine("Options:");
             optionSet.WriteOptionDescriptions(Console.Out);
             Console.WriteLine();
-            Console.WriteLine("By default, the Carbon backend is used.");
+            Console.WriteLine("By default, no backend is used (only translation is run).");
         }
 
         private static ExitCode RunVerification(
