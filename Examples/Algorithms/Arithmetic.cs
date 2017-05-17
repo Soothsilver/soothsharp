943f2b5c4fd2036bbad085b7ac68b4107e04e601
Petr Hudeček <petrhudecek2010@gmail.com>
2017-05-17 16:45:28 +0200

    Hello again, dear project! Long time, no see!

---
+++ Examples/Algorithms/Arithmetic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Soothsharp.Contracts;
+
+namespace Soothsharp.Examples.Algorithms
+{
+    public class Arithmetic
+    {
+        /// <summary>
+        /// Returns the greater of two numbers.
+        /// </summary>
+        /// <param name="a">The first number to compare.</param>
+        /// <param name="b">The second number to compare.</param>
+        /// <returns>The greater number.</returns>
+        public static int Max(int a, int b)
+        {
+            Contract.Ensures(a > b ? Contract.IntegerResult == a : Contract.IntegerResult == b);
+
+            if (a < b)
+            {
+                return b;
+            }
+            else
+            {
+                return a;
+            }
+        }
+
+        /// <summary>
+        /// Returns the lesser of two numbers.
+        /// </summary>
+        /// <param name="a">The first number to compare.</param>
+        /// <param name="b">The second number to compare.</param>
+        /// <returns>The lesser number.</returns>
+        public static int Min(int a, int b)
+        {
+            Contract.Ensures(a < b ? Contract.IntegerResult == a : Contract.IntegerResult == b);
+
+            if (a == Max(a, b))
+            {
+                return b;
+            }
+            else
+            {
+                return a;
+            }
+        }
+    }
+}
