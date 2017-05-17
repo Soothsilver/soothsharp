678dd154017848c0690f2b28007367a7f27f4b78
Petr Hudeček <petrhudecek2010@gmail.com>
2017-05-17 18:45:39 +0200

    getting back in the swing of things

--- Soothsharp.Translation/TranslationProcessResult.cs
+++ Soothsharp.Translation/TranslationProcessResult.cs
@@ -5,9 +5,22 @@ using Soothsharp.Translation.Trees.Silver;
 
 namespace Soothsharp.Translation
 {
+    /// <summary>
+    /// Represents all the results of a single run of a translation process. This is used by the front-end, by the plugin,
+    /// and by automated tests.
+    /// </summary>
     public class TranslationProcessResult
     {
-        public Silvernode Silvernode;
+        /// <summary>
+        /// Gets the Viper tree that's the result of the translation. May be null if the translation failed totally, such
+        /// as if the source files could not be opened.
+        /// </summary>
+        public Silvernode Silvernode { get; }
+
+        /// <summary>
+        /// Gets a value that indicates whether the translation completed without any errors. If that's the case, 
+        /// <see cref="Silvernode"/> is guaranteed to be non-null. 
+        /// </summary>
         public bool WasTranslationSuccessful
         {
             get
@@ -15,12 +28,18 @@ namespace Soothsharp.Translation
                 return this.Errors.All(err => err.Diagnostic.Severity != DiagnosticSeverity.Error);
             }
         }
+
+        /// <summary>
+        /// Gets the list of errors triggered during the translation process.
+        /// </summary>
         public List<Error> Errors { get; } 
+
         public TranslationProcessResult(Silvernode silvernode, List<Error> errors)
         {
             this.Silvernode = silvernode;
             this.Errors = errors;
         }
+
         public static TranslationProcessResult Error(SyntaxNode syntaxNode, SoothsharpDiagnostic diagnostic, params object[] arguments)
         {
             return new TranslationProcessResult(null, new List<Error> { new Error(diagnostic, syntaxNode, arguments) });
